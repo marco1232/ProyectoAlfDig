4a4d04182a167c4c52ccf98c4b3743d216aefb26
marco1232 <33439544+marco1232@users.noreply.github.com>
2019-06-20 12:37:17 -0500

    mejoras
    xd

--- bdalfabetizacion.sql
+++ bdalfabetizacion.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 15-06-2019 a las 05:00:05
+-- Tiempo de generación: 20-06-2019 a las 16:57:30
 -- Versión del servidor: 5.7.23
 -- Versión de PHP: 7.2.10
 
@@ -93,6 +93,35 @@ INSERT INTO `colaborador` (`id_cola`, `dniusu`, `cip`, `gradoacad`, `especia`, `
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `historia`
+--
+
+DROP TABLE IF EXISTS `historia`;
+CREATE TABLE IF NOT EXISTS `historia` (
+  `id_his` int(11) NOT NULL,
+  `dniusu` char(8) NOT NULL,
+  `fechpar` text NOT NULL,
+  `img_perf` varchar(200) NOT NULL,
+  PRIMARY KEY (`id_his`),
+  KEY `asd` (`dniusu`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `historia`
+--
+
+INSERT INTO `historia` (`id_his`, `dniusu`, `fechpar`, `img_perf`) VALUES
+(1, '76755672', '// 2019-05-06 // 2019-05-07\r\n// 2019-05-20 //\r\n\r\n', ''),
+(2, '15123151', '// 2019-05-06 // 2019-05-07 //\r\n', ''),
+(3, '76317263', '// 2019-05-06 //', ''),
+(4, '74209553', '// 2019-05-06 // 2019-05-07\r\n// 2019-05-20 //', ''),
+(5, '65231235', '// 2019-05-06 // 2019-05-07\r\n// 2019-05-20 //', ''),
+(6, '34567897', 'no participo', ''),
+(7, '81293127', '// 2019-05-06 // 2019-05-07\r\n// 2019-05-20 //', '');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `materiales`
 --
 
@@ -154,6 +183,9 @@ CREATE TABLE IF NOT EXISTS `usuario` (
   `disponibilidad` char(98) NOT NULL,
   `estado` set('INACTIVO','ACTIVO','PENDIENTE') DEFAULT NULL,
   `img_perf` varchar(200) NOT NULL,
+  `fechapar` date DEFAULT NULL,
+  `partfecha` date DEFAULT NULL,
+  `fechaparti` date DEFAULT NULL,
   PRIMARY KEY (`dniusu`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -161,21 +193,21 @@ CREATE TABLE IF NOT EXISTS `usuario` (
 -- Volcado de datos para la tabla `usuario`
 --
 
-INSERT INTO `usuario` (`dniusu`, `usua`, `pass`, `perf`, `apellidos`, `nombres`, `provnaci`, `distnaci`, `fecnaci`, `domiact`, `distact`, `email`, `celu`, `tele`, `nomempres`, `cargempres`, `dirempres`, `distempres`, `telempres`, `emailempres`, `participaprevia`, `objetencasa`, `enterarcampa`, `disponibilidad`, `estado`, `img_perf`) VALUES
-('14141231', 'd', '123', 'Apoyo', 'Prudencio', 'Daniela', 'Lima', 'Lima', '1994-10-07', 'Chorrillos', 'Chorrillos', 'daniela@hotmail.com', '976283647', '7628323', '', '', '', '', '', '', 'No', '1110', '010100000001', '00000000000000000000000000000000001110000000000000000000000000000000000000000000000000000000000000', 'PENDIENTE', '307717.png'),
-('15123151', 'jcardenas', '123', 'Beneficiario', 'Cardenas Dario', 'Samuel Luis', 'Lima', 'Lima', '1994-11-11', 'Villa el Salvador', 'Villa El Salvador', 'jose@hotmail.com', '976563722', '652637', '', '', '', '', '', '', 'Si', '1010', '000111000000', '00000000000000000000000000000011111100000000000000000000000000000000000000000000000000000000000000', 'PENDIENTE', 'imgperf2.png'),
-('34567897', 'joscco', '123', 'Capacitador', 'oscco rincon', 'jayro', 'Lima', 'Lima', '1995-01-01', 'San Martin de your mama', 'SJL', 'jayro@hotmail.com', '946821574', '614821125', '', '', '', '', '', '', 'No', '1010', '010000001000', '11000111010100111111111000010001101110110101010010000011010111101100011010001110001000000001111100', 'ACTIVO', ''),
-('61412412', 'rcayllahua', '123', 'Capacitador', 'Cayllahua Zela', 'Rolly', 'Lima', 'Lima', '0000-00-00', 'Lince', 'Lince', 'rolly@hotmail.com', '', '', '', '', '', '', '', '', 'No', '1100', '000000001010', '00000000000000000000000000000000011111000000000000000000000000000000000000000000000000000000000000', 'PENDIENTE', ''),
-('65231235', 'mgamboa', '123', 'Capacitador', 'Gamboa Ramoz', 'Miriam', 'Lima', 'Lurin', '1997-07-05', 'Deposito de gas N3', 'Lurin', 'migara@hotmail.com', '977231239', '6786312', '', '', '', '', '', '', 'No', '1111', '110000000100', '11011110100111110110110101010011001101010001000010000100010110110001011100110111001001110001010001', 'ACTIVO', ''),
-('72191725', 'admin', 'admin', 'Administrador', 'Gomez Noa', 'Ricardo Yvan', 'Lima', 'Lima', '1999-04-30', 'Av.Jose de San Martin', 'Villa Maria del Triunfo', 'yvan_gomez30@hotmail.com', '992955138', '2830281', NULL, NULL, NULL, NULL, NULL, NULL, 'No', '1111', '100101010100', '01110101111001000101000001100011001100100110010000111000110100111101101101111011101000101101011011', 'ACTIVO', ''),
-('74209553', 'dmaytam', '123', 'Capacitador', 'Mayta Matienso', 'Dayanne Genesis', 'Lima', 'Lurin', '1997-02-11', 'Av Venezuela Mz 27 A Lt 1 - KM40', 'Lurin', 'dayanne_97_15@hotmail.com', '986262578', '6123235', '', '', '', '', '', '', 'No', '1111', '100001001010', '11111010010000111001011001101100000110001101011101110101010000101101111000110100110101101011110111', 'ACTIVO', ''),
-('76317263', 'jalmenio', '123', 'Beneficiario', 'Almenio Ruer', 'Julio Schab', 'Lima', 'Lima', '1995-07-09', 'Chorrillos', 'Chorrillos', 'julio@hotmail.com', '', '', '', '', '', '', '', '', 'Si', '1110', '011100000001', '00000000000000000000000000000011111000000000000000000000000000000000000000000000000000000000000000', 'PENDIENTE', 'imgperf1.png'),
-('76755672', 'hinga', '123', 'Beneficiario', 'Inga Arteaga', 'Hernan Manuel', 'Chiclayo', 'Chiclayo', '1995-01-14', 'Los claveles', 'Lurin', 'hm_inga@hotmail.com', '976283647', '7628323', '', '', '', '', '', '', 'No', '1111', '010000001000', '01011010010000110001011001101100100111001101010101110101010000100101111000110100110101001010010111', 'ACTIVO', 'imgperf1.png'),
-('81293127', 'jamachi', '123', 'Capacitador', 'amachi choque', 'john', 'Lima', 'Lima', '1995-09-02', 'Chorrillos', 'Chorrillos', 'amachi@hotmail.com', '956487647', '5666465', '', '', '', '', '', '', 'No', '1111', '000000000010', '00000000000000000000000000000000011111000000000000000000000000000000000000000000000000000000000000', 'PENDIENTE', ''),
-('81738172', 'adurand', '123', 'Capacitador', 'Durand Quispe', 'Alexis', 'Lima', 'Lima', '1998-03-26', 'Ate', 'Ate', 'alexis@hotmail.com', '985412451', '5156512', '', '', '', '', '', '', 'No', '0101', '010000000011', '00000000000000000000000000000000111111000000000000000000000000000000000000000000000000000000000000', 'PENDIENTE', ''),
-('87613772', 'ftra', '123', 'Apoyo', 'Tra Pito', 'Felix El', 'Lima', 'Lima', '1994-06-10', 'Chorrillos', 'Chorrillos', 'felix@hotmail.com', '988763272', '653261', '', '', '', '', '', '', 'No', '1100', '011100000001', '00111111000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000', 'PENDIENTE', ''),
-('88617376', 'pcusi', '123', 'Capacitador', 'Cusi Ruiz', 'Piero', 'Lima', 'Lima', '1996-08-03', 'SLM', 'San Juan de Miraflores', 'cusi@hotmail.com', '', '', '', '', '', '', '', '', 'No', '1111', '000000000010', '00000000000000000000000000000000011111000000000000000000000000000000000000000000000000000000000000', 'PENDIENTE', ''),
-('99999999', 'jmayta', '123', 'Asistente', 'Mayta Matienso', 'Jose Felipe', 'Lima', 'Lurin', '1992-11-22', 'KM40', 'Lurin', 'fepile@hotmail.com', '983938272', '7636378', '', '', '', '', '', '', 'No', '1111', '001000000001', '00000000000000000000111000000000000000000000000000000000000000000000000000000000000000000000000000', 'PENDIENTE', '');
+INSERT INTO `usuario` (`dniusu`, `usua`, `pass`, `perf`, `apellidos`, `nombres`, `provnaci`, `distnaci`, `fecnaci`, `domiact`, `distact`, `email`, `celu`, `tele`, `nomempres`, `cargempres`, `dirempres`, `distempres`, `telempres`, `emailempres`, `participaprevia`, `objetencasa`, `enterarcampa`, `disponibilidad`, `estado`, `img_perf`, `fechapar`, `partfecha`, `fechaparti`) VALUES
+('14141231', 'd', '123', 'Apoyo', 'Prudencio', 'Daniela', 'Lima', 'Lima', '1994-10-07', 'Chorrillos', 'Chorrillos', 'daniela@hotmail.com', '976283647', '7628323', '', '', '', '', '', '', 'No', '1110', '010100000001', '00000000000000000000000000000000001110000000000000000000000000000000000000000000000000000000000000', 'PENDIENTE', '307717.png', '2019-05-19', '2019-05-20', '2019-05-21'),
+('15123151', 'jcardenas', '123', 'Beneficiario', 'Cardenas Dario', 'Samuel Luis', 'Lima', 'Lima', '1994-11-11', 'Villa el Salvador', 'Villa El Salvador', 'jose@hotmail.com', '976563722', '652637', '', '', '', '', '', '', 'Si', '1010', '000111000000', '00000000000000000000000000000011111100000000000000000000000000000000000000000000000000000000000000', 'PENDIENTE', 'imgperf2.png', NULL, NULL, NULL),
+('34567897', 'joscco', '123', 'Capacitador', 'oscco rincon', 'jayro', 'Lima', 'Lima', '1995-01-01', 'San Martin de your mama', 'SJL', 'jayro@hotmail.com', '946821574', '614821125', '', '', '', '', '', '', 'No', '1010', '010000001000', '11000111010100111111111000010001101110110101010010000011010111101100011010001110001000000001111100', 'ACTIVO', '', NULL, NULL, NULL),
+('61412412', 'rcayllahua', '123', 'Capacitador', 'Cayllahua Zela', 'Rolly', 'Lima', 'Lima', '0000-00-00', 'Lince', 'Lince', 'rolly@hotmail.com', '', '', '', '', '', '', '', '', 'No', '1100', '000000001010', '00000000000000000000000000000000011111000000000000000000000000000000000000000000000000000000000000', 'PENDIENTE', '', NULL, NULL, NULL),
+('65231235', 'mgamboa', '123', 'Capacitador', 'Gamboa Ramoz', 'Miriam', 'Lima', 'Lurin', '1997-07-05', 'Deposito de gas N3', 'Lurin', 'migara@hotmail.com', '977231239', '6786312', '', '', '', '', '', '', 'No', '1111', '110000000100', '11011110100111110110110101010011001101010001000010000100010110110001011100110111001001110001010001', 'ACTIVO', '', NULL, NULL, NULL),
+('72191725', 'admin', 'admin', 'Administrador', 'Gomez Noa', 'Ricardo Yvan', 'Lima', 'Lima', '1999-04-30', 'Av.Jose de San Martin', 'Villa Maria del Triunfo', 'yvan_gomez30@hotmail.com', '992955138', '2830281', NULL, NULL, NULL, NULL, NULL, NULL, 'No', '1111', '100101010100', '01110101111001000101000001100011001100100110010000111000110100111101101101111011101000101101011011', 'ACTIVO', '', NULL, NULL, NULL),
+('74209553', 'dmaytam', '123', 'Capacitador', 'Mayta Matienso', 'Dayanne Genesis', 'Lima', 'Lurin', '1997-02-11', 'Av Venezuela Mz 27 A Lt 1 - KM40', 'Lurin', 'dayanne_97_15@hotmail.com', '986262578', '6123235', '', '', '', '', '', '', 'No', '1111', '100001001010', '11111010010000111001011001101100000110001101011101110101010000101101111000110100110101101011110111', 'ACTIVO', '', NULL, NULL, NULL),
+('76317263', 'jalmenio', '123', 'Beneficiario', 'Almenio Ruer', 'Julio Schab', 'Lima', 'Lima', '1995-07-09', 'Chorrillos', 'Chorrillos', 'julio@hotmail.com', '', '', '', '', '', '', '', '', 'Si', '1110', '011100000001', '00000000000000000000000000000011111000000000000000000000000000000000000000000000000000000000000000', 'PENDIENTE', 'imgperf1.png', NULL, NULL, NULL),
+('76755672', 'hinga', '123', 'Beneficiario', 'Inga Arteaga', 'Hernan Manuel', 'Chiclayo', 'Chiclayo', '1995-01-14', 'Los claveles', 'Lurin', 'hm_inga@hotmail.com', '976283647', '7628323', '', '', '', '', '', '', 'No', '1111', '010000001000', '01011010010000110001011001101100100111001101010101110101010000100101111000110100110101001010010111', 'ACTIVO', 'imgperf1.png', '2019-05-19', '2019-05-21', '2019-06-01'),
+('81293127', 'jamachi', '123', 'Capacitador', 'amachi choque', 'john', 'Lima', 'Lima', '1995-09-02', 'Chorrillos', 'Chorrillos', 'amachi@hotmail.com', '956487647', '5666465', '', '', '', '', '', '', 'No', '1111', '000000000010', '00000000000000000000000000000000011111000000000000000000000000000000000000000000000000000000000000', 'PENDIENTE', '', NULL, NULL, NULL),
+('81738172', 'adurand', '123', 'Capacitador', 'Durand Quispe', 'Alexis', 'Lima', 'Lima', '1998-03-26', 'Ate', 'Ate', 'alexis@hotmail.com', '985412451', '5156512', '', '', '', '', '', '', 'No', '0101', '010000000011', '00000000000000000000000000000000111111000000000000000000000000000000000000000000000000000000000000', 'PENDIENTE', '', NULL, NULL, NULL),
+('87613772', 'ftra', '123', 'Apoyo', 'Tra Pito', 'Felix El', 'Lima', 'Lima', '1994-06-10', 'Chorrillos', 'Chorrillos', 'felix@hotmail.com', '988763272', '653261', '', '', '', '', '', '', 'No', '1100', '011100000001', '00111111000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000', 'PENDIENTE', '', NULL, NULL, NULL),
+('88617376', 'pcusi', '123', 'Capacitador', 'Cusi Ruiz', 'Piero', 'Lima', 'Lima', '1996-08-03', 'SLM', 'San Juan de Miraflores', 'cusi@hotmail.com', '', '', '', '', '', '', '', '', 'No', '1111', '000000000010', '00000000000000000000000000000000011111000000000000000000000000000000000000000000000000000000000000', 'PENDIENTE', '', NULL, NULL, NULL),
+('99999999', 'jmayta', '123', 'Asistente', 'Mayta Matienso', 'Jose Felipe', 'Lima', 'Lurin', '1992-11-22', 'KM40', 'Lurin', 'fepile@hotmail.com', '983938272', '7636378', '', '', '', '', '', '', 'No', '1111', '001000000001', '00000000000000000000111000000000000000000000000000000000000000000000000000000000000000000000000000', 'PENDIENTE', '', NULL, NULL, NULL);
 
 --
 -- Restricciones para tablas volcadas
@@ -192,6 +224,12 @@ ALTER TABLE `beneficiario`
 --
 ALTER TABLE `colaborador`
   ADD CONSTRAINT `dnicola_fk_1` FOREIGN KEY (`dniusu`) REFERENCES `usuario` (`dniusu`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `historia`
+--
+ALTER TABLE `historia`
+  ADD CONSTRAINT `asd` FOREIGN KEY (`dniusu`) REFERENCES `usuario` (`dniusu`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Filtros para la tabla `materiales`
